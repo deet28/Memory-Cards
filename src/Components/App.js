d8d3ecb40ddf579a01b3855f3a672905c4ab9429
David Turchetta <davidturchetta@outlook.com>
2022-01-17 13:53:56 -0500

    Setting up page display

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import '../css/App.css';
+import Card from './Cards';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Memory Cards</h1>
+      </header>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Cards.js
@@ -0,0 +1,19 @@
+import { createFactory } from "react";
+
+import React from 'react'
+
+export default function Cards() {
+  return (
+    <div className = "card--main-div">
+      <div className = "card--div"></div>
+      <div className = "card--div"></div>
+      <div className = "card--div"></div>
+      <div className = "card--div"></div>
+      <div className = "card--div"></div>
+      <div className = "card--div"></div>
+      <div className = "card--div"></div>
+      <div className = "card--div"></div>
+      <div className = "card--div"></div>
+    </div>
+  )
+}
