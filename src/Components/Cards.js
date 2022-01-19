55dd45289f811003859e938781ed3d9c49d19ba1
David Turchetta <davidturchetta@outlook.com>
2022-01-19 17:18:28 -0500

    Set up counter variable for current and high score. Counters both increment appropriately and are displayed on page.

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -46,18 +46,44 @@ export default function Cards() {
     {name:`Popo`,src:popo,id:uuidv4()},
     {name:`Shenron`,src:shenron,id:uuidv4()},
     {name:`Yamcha`,src:yamcha,id:uuidv4()},
-    {name:`Tien`,src:Tien,id:uuidv4()},
+    {name:`Tien`,src:tien,id:uuidv4()},
     {name:`Yajirobe`,src:yajirobe,id:uuidv4()},
     {name:`Piccolo`,src:piccolo,id:uuidv4()},
     {name:`Mai`,src:mai,id:uuidv4()},
     {name:`Jackie Chun`,src:jackiechun,id:uuidv4()},
     {name:`Pilaf`,src:pilaf,id:uuidv4()},
     {name:`Murasaki`,src:murasaki,id:uuidv4()}
-  ];
-
-  let shuffledArray = cardsArray.filter((index,i)=>i<6);  
+  ]; 
+  
   const [cards, setCards] = useState(cardsArray.filter((index,i)=>i<6));
+  const [curscore, setCurScore] = useState(0);
+  const [hiscore,setHiScore] = useState(0);
+  const [arrayTrack,setArrayTrack] = useState([])
 
+  
+function handleClick(e){
+    pickCard(e);
+    shuffleDeck();
+  }
+
+function pickCard(e){
+  for(let i = 0; i < cardsArray.length; i++){
+    if (e.target.name === cardsArray[i].name){
+      if (arrayTrack.includes(e.target.name)==false){
+        arrayTrack.push(e.target.name);
+        setCurScore(curscore+1)
+        console.log(arrayTrack);
+      } else 
+      if (arrayTrack.includes(e.target.name)==true){
+        checkScore(); 
+        setCurScore(curscore-curscore);
+        setArrayTrack([]);
+        console.log(arrayTrack)
+      }
+    }  
+  }
+}
+  
   function shuffleDeck(){
     let i = cardsArray.length -1;
     for(; i>0 ;i--){
@@ -66,22 +92,54 @@ export default function Cards() {
       cardsArray[i] = cardsArray[j];
       cardsArray[j] = temp
     }
-    shuffledArray = cardsArray.filter((index,i) => i < 6);
+    const shuffledArray = cardsArray.filter((index,i) => i < 6);
     setCards(shuffledArray)
   }
 
+  function checkScore(){
+    if(curscore > hiscore){
+      setHiScore(curscore);
+    }
+  }
+  
   return (
     <>
     <div className = "count--counter-div">
-      <div className = "count--high-score"></div>
+      <div className = "count--high-score">High Score:{hiscore}</div>
+      <div className = "count--current-score">Current Score:{curscore}</div>
     </div>
     <div className = "card--main-div" key = "50">
       {cards.map((index => (
         <div className = "card--div" key = {index.src}>
-          <img src = {index.src} key = {index.id} onClick = {shuffleDeck}></img>
+          <img 
+          src = {index.src} 
+          key = {index.id} 
+          name = {index.name}
+          onClick = {handleClick}></img>
         </div>
       )))}
     </div>
     </>
   )
 }
+
+
+
+//function selectedCard(e){
+//  for(let i = 0; i < cardsArray.length; i++){
+//    if (e.target.name === cardsArray[i].name){
+//      if(cardsArray[i].selected == true){
+//        setCurScore(curscore-curscore)
+//      } else if (cardsArray[i].selected == false) {
+//      cardsArray[i].selected = true;
+//      setCurScore(curscore+1)
+//    }
+//  }
+//}
+//}
+
+//else if (arrayTrack.includes(e.target.name)==true){
+//  setArrayTrack([]);
+//  setCurScore(curscore-curscore);
+//  console.log(arrayTrack)
+//}
